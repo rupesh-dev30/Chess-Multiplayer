c2424673bdb84f53cbe7f7db9e0985b785cac61b
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-04 02:34:36 +0530

    Socket Connect Frontend and Backend

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-const ex
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const path = require("path");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {title: "Welcome to Chess Game"});
+});
+
+io.on("connection", (uniqueSocket) => {
+  console.log("connected");
+})
+
+server.listen(3000, () => {
+  console.log("Server is running at port 3000");
+})
